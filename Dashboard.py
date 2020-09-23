eafe71bf9dba23735995cdbe8531690f3513b4f7
David Rocha <davidgr.bio@gmail.com>
2020-09-23 17:34:26 -0300

    groupby point. ecoli mean()

--- Dashboard.py
+++ Dashboard.py
@@ -2,4 +2,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-df = pd.read_csv('df.csv', sep=';').iloc[:, 1:]
+df = pd.read_csv('df.csv', sep=';', index_col=0)
+
+e_coli_ponto = df.groupby(['ponto', 'agua_doce', 'desembocadura_praia', 'ponto_perto_desembocadura', 'lat', 'long'], as_index=False)['E.Coli NMP*/100ml'].mean()
