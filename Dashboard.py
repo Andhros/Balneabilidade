10c6879dc5c475bbc4dccec820d40734c8ca0ec4
Andhros <andhros@usp.br>
2020-09-24 14:53:42 -0300

    Inicio Dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+import statsmodels
 
-df = pd.read_csv('df.csv', sep=';').iloc[:, 1:]
+df = pd.read_csv('df.csv', sep=';', index_col=0)
+sns.heatmap(
+    df.corr(),
+    vmin=-1, vmax=1, center=0,
+    cmap=sns.diverging_palette(20, 220, n=200),
+    square=True
+)
